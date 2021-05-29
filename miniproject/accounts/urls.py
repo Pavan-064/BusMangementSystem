67adfd8c61205ffcf99ac865ce171f96e5259af5
Pavan-064 <64072484+Pavan-064@users.noreply.github.com>
2021-05-30 01:30:40 +0530

    Delete miniproject/accounts directory

--- miniproject/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-   path('studentregistration',views.studentregistration,name = 'studentregistration'),
-   path('studentlogin',views.studentlogin,name = 'studentlogin'),
-   
-   
-]
